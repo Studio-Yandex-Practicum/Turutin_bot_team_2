d73f1b592eae3803813037a005da68fe750cf867
Стас Басов <stas.basov@whatever.com>
2024-11-07 23:03:31 +0300

    изменить размеры кнопки бота

--- src/bot_app/buttons.py
+++ src/bot_app/buttons.py
@@ -9,12 +9,13 @@ def start_keyboard() -> ReplyKeyboardMarkup:
     """Возвращает клавиатуру с кнопками 'Создать заявку' и 'Мои заявки'."""
     return ReplyKeyboardMarkup(
         [['Создать заявку', 'Мои заявки']],
-        one_time_keyboard=True,
+        one_time_keyboard=True, resize_keyboard=False
     )
 
 
 def contact_keyboard() -> InlineKeyboardMarkup:
     """Создает кнопку для начала нового опроса."""
     return InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Да", callback_data="start_survey")]],
+        [[InlineKeyboardButton("Да", callback_data="start_survey",
+                               resize_keyboard=False)]],
     )
