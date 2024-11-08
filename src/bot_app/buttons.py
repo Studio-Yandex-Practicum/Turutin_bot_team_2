2eba361419990bee174a8e9a2a3922b842230754
Стас Басов <stas.basov@whatever.com>
2024-11-08 07:37:24 +0300

    добавить поле name,и изменить поле timestamp в ApplicationCheckStatus

--- src/bot_app/buttons.py
+++ src/bot_app/buttons.py
@@ -8,14 +8,11 @@ from telegram import (
 def start_keyboard() -> ReplyKeyboardMarkup:
     """Возвращает клавиатуру с кнопками 'Создать заявку' и 'Мои заявки'."""
     return ReplyKeyboardMarkup(
-        [['Создать заявку', 'Мои заявки']],
-        one_time_keyboard=True, resize_keyboard=False
-    )
+        [['Создать заявку', 'Мои заявки']], one_time_keyboard=True)
 
 
 def contact_keyboard() -> InlineKeyboardMarkup:
     """Создает кнопку для начала нового опроса."""
     return InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Да", callback_data="start_survey",
-                               resize_keyboard=False)]],
+        [[InlineKeyboardButton("Да", callback_data="start_survey")]],
     )
